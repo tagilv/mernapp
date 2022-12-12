24f1c39e8ff7a4ba858fc77ed4f6c09692a2da8f
Viktor Tagil <viktor.tagil@gmail.com>
2022-12-12 12:31:08 +0100

    resp navbar working

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,19 +1,107 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
+import { FaBars } from "react-icons/fa";
+
 function Nav() {
   const { user } = useContext(AuthContext);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setMobileMenuOpen((prev) => !prev);
+  };
+
+  // Implement later
+  // const closeMenu = () => {
+  //   setMobileMenuOpen(false);
+  // };
+
   return (
-    <nav>
-      <>
-        {user && <p>Hi you {user.email}</p>}
-        <h2>Nav</h2> <Link to="/">Home</Link> | <Link to="/weeks">Weeks</Link>|{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/register">register</Link> | <Link to="/login">login</Link>
-      </>
+    <nav className="bg-gray-200">
+      <div className="max-w-6xl mx-auto px-5">
+        <div className="flex justify-between">
+          <div className="flex space-x-3">
+            {/* logo */}
+            <div className="flex items-center py-4 px-1  text-gray-700 hover:text-gray-900">
+              <Link to="/">Logo</Link>
+              {/* <span className="font-bold">Ryggskolan</span> */}
+            </div>
+
+            {/* primary nav */}
+            <div className="hidden md:flex items-center space-x-4">
+              <Link
+                className="py-4 px-1 text-black text-gray-700 hover:text-gray-900"
+                to="/weeks"
+              >
+                Treatment Plan
+              </Link>
+            </div>
+          </div>
+          {/* secondary nan */}
+          <div className="hidden md:flex items-center space-x-1">
+            <Link className="py-5 px-1" to="/profile">
+              Profile
+            </Link>
+            <Link
+              className="py-1 px-3 bg-yellow-400 hover:bg-yellow-300 text-yellow-800 hover:text-yellow-800 rounded shadow transition duration-400"
+              to="/login"
+            >
+              login
+            </Link>
+            {/* <p>{user.email}</p> */}
+          </div>
+
+          {/* Mobile button goes here */}
+          <div className="md:hidden flex items-center">
+            {/* <button onClick={handleToggle}>
+              {mobileMenuOpen ? "Close" : "Open"}
+            </button>
+            <ul className={`menuNav ${mobileMenuOpen ? " showMenu" : ""}`}>
+            </ul> */}
+            <button
+              onClick={handleToggle}
+              className="mobile-menu-button"
+              type=""
+            >
+              <FaBars className="w-6 h-6" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile menu*/}
+      <div className="md:hidden">
+        <Link
+          className={`mobile-menu ${
+            mobileMenuOpen
+              ? "block py-4 px-4 text-sm hover:bg-gray-300"
+              : "hidden block py-4 px-4 text-sm hover:bg-gray-300"
+          }`}
+          to="/weeks"
+        >
+          Treatment Plan
+        </Link>
+        <Link
+          className={`mobile-menu ${
+            mobileMenuOpen
+              ? "block py-4 px-4 text-sm hover:bg-gray-300"
+              : "hidden block py-4 px-4 text-sm hover:bg-gray-300"
+          }`}
+          to="/profile"
+        >
+          Profile
+        </Link>
+      </div>
     </nav>
   );
 }
 
 export default Nav;
+
+{
+  /* {user && <p>Hi you {user.email}</p>}
+          <h2>Nav</h2> <Link to="/">Home</Link> | <Link to="/weeks">Weeks</Link>
+          | <Link to="/profile">Profile</Link> |{" "}
+          <Link to="/register">register</Link> | <Link to="/login">login</Link> */
+}
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -3,7 +3,8 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h2>Home</h2>
+      <h2 className="text-3xl font-bold underline">Home</h2>
+      <h2>test</h2>
     </div>
   );
 }
